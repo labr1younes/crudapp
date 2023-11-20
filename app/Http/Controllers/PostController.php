846f2e1ba7ee1fe4fbd5154bbfa5219635e3ffe2
Ahmed <younesetud@Gmail.Com>
2023-11-20 09:36:28 +0100

    fixed some errors

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,8 +14,8 @@ class PostController extends Controller
             'title' => 'required',
             'body' => 'required'
         ]) ;
-        $incomingfields['title'] = strip_tags(incomingfields['title']);
-        $incomingfields['body'] = strip_tags(incomingfields['body']);
+        $incomingfields['title'] = strip_tags($incomingfields['title']);
+        $incomingfields['body'] = strip_tags($incomingfields['body']);
         $incomingfields['user_id'] = auth()->id();
         Post::create($incomingfields);
         return redirect('/');
